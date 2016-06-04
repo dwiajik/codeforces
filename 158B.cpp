bab060f120f3acee1fd98213ff4e2fc8f4099312
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2016-06-05 00:58:19 +0700

    158B solution fixed, add .gitignore

--- 158B.cpp
+++ 158B.cpp
@@ -1,48 +1,41 @@
 #include <iostream>
 
+using namespace std;
+
 main()
 {
-	int n, result, temp;
-	std::cin >> n;
-	int x[4] = {0,0,0,0};
-	for(int i = 0; i < n; i++)
-	{
-		std::cin >> temp;
-		if(temp == 4)x[3]++;
-		else if(temp == 3)x[2]++;
-		else if(temp == 2)x[1]++;
-		else if(temp == 1)x[0]++;
-	}
-	
-	result += x[3] + x[2];
-	result += x[1]/2;
-	
-	if(x[2] > x[0])
-	{
-		if(x[1]%2==1)
-		{
-			result+=1;
-		}
-	}
-	else
-	{
-		result += (x[0]-x[2])/4;
-		if(x[1]%2==1)
-		{
-			result+=1;
-			if( (x[0]-x[2] - 2) > 0 && (x[0]-x[2] - 2)%4 != 0)
-			{
-				result +=1;
-			}
-		}
-		else
-		{
-			if((x[0]-x[2] - 2) > 0 && (x[0]-x[2])%4 != 0)
-			{
-				result +=1;
-			}
-		}
-	}
-	
-	std::cout << result;
+    int n;
+    cin >> n;
+
+    int x[4] = {0,0,0,0};
+
+    for (int i = 0; i < n; i++)
+    {
+        int nChild;
+        cin >> nChild;
+        switch (nChild)
+        {
+            case 1:
+                x[0]++;
+                break;
+            case 2:
+                x[1]++;
+                break;
+            case 3:
+                x[2]++;
+                break;
+            case 4:
+                x[3]++;
+                break;
+            default:
+                break;
+        }
+    }
+
+    int temp1 = (x[0] - x[2]) >= 0? x[0] - x[2]: 0;
+//    cout << temp1 << endl;
+    int temp2 = (2 * x[1] + temp1) % 4 == 0? (2 * x[1] + temp1) / 4: ((2 * x[1] + temp1) / 4) + 1;
+//    cout << temp2 << endl;
+
+    cout << x[3] + x[2] + temp2 << endl;
 }
