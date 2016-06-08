b2b01b4aa9c2572b48adda38bf6db6cea1cdd9bf
Dwi Aji Kurniawan <dwi.aji.kurniawan@gmail.com>
2016-06-08 08:07:39 +0700

    Add 122A, 160A, 281A code using Python

---
+++ 160A.py
@@ -0,0 +1,16 @@
+n = input()
+coins = input().split()
+coins = list(map(int, coins))
+coins.sort(reverse=True)
+avg = sum(coins)/2
+
+count = 0
+ownCoins = 0
+
+for coin in coins:
+	ownCoins += coin
+	count += 1
+	if ownCoins > avg:
+		break
+
+print(count)
